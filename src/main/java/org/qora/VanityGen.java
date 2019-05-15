d6148eb2e4748ba1d0e42beaa5e52ba50ea0e8a7
catbref <misc-github@talk2dom.com>
2019-05-15 11:00:53 +0100

    Add mnemonic phrase support to VanityGen

--- src/main/java/org/qora/VanityGen.java
+++ src/main/java/org/qora/VanityGen.java
@@ -8,7 +8,11 @@ import java.util.Random;
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import org.bouncycastle.jsse.provider.BouncyCastleJsseProvider;
 import org.qora.account.PrivateKeyAccount;
+import org.qora.crypto.Crypto;
+import org.qora.utils.BIP39;
 import org.qora.utils.Base58;
+
+import com.google.common.primitives.Bytes;
 
 public class VanityGen {
 
@@ -23,14 +27,25 @@ public class VanityGen {
 		Security.insertProviderAt(new BouncyCastleJsseProvider(), 1);
 
 		Random random = new SecureRandom();
-		byte[] seed = new byte[32];
+		byte[] entropy = new byte[16];
 
 		while (true) {
-			random.nextBytes(seed);
-			PrivateKeyAccount account = new PrivateKeyAccount(null, seed);
+			// Generate entropy internally
+			random.nextBytes(entropy);
+
+			// Use SHA256 to generate more bits
+			byte[] hash = Crypto.digest(entropy);
+
+			// Append first 4 bits from hash to end. (Actually 8 bits but we only use 4).
+			byte checksum = (byte) (hash[0] & 0xf0);
+			byte[] entropy132 = Bytes.concat(entropy, new byte[] { checksum });
+
+			String mnemonic = BIP39.encode(entropy132, "en");
+
+			PrivateKeyAccount account = new PrivateKeyAccount(null, hash);
 
 			if (account.getAddress().startsWith(argv[0]))
-				System.out.println(String.format("Address: %s, public key: %s, private key: %s", account.getAddress(), Base58.encode(account.getPublicKey()), Base58.encode(seed)));
+				System.out.println(String.format("Address: %s, public key: %s, private key: %s, mnemonic: %s", account.getAddress(), Base58.encode(account.getPublicKey()), Base58.encode(hash), mnemonic));
 		}
 	}
 
