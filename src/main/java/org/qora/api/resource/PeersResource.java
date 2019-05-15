6e03e860ec14179cc9f7a5d4711c81c6ffc396bd
catbref <misc-github@talk2dom.com>
2019-05-15 20:47:34 +0100

    Fix PROXY_FORGING transaction length issue + correct port numbers in API docs

--- src/main/java/org/qora/api/resource/PeersResource.java
+++ src/main/java/org/qora/api/resource/PeersResource.java
@@ -111,10 +111,10 @@ public class PeersResource {
 	@POST
 	@Operation(
 		summary = "Add new peer address",
-		description = "Specify a new peer using hostname, IPv4 address, IPv6 address and optional port number preceeded with colon (e.g. :9084)<br>"
+		description = "Specify a new peer using hostname, IPv4 address, IPv6 address and optional port number preceeded with colon (e.g. :9889)<br>"
 				+ "Note that IPv6 literal addresses must be surrounded with brackets.<br>" + "Examples:<br><ul>" + "<li>some-peer.example.com</li>"
-				+ "<li>some-peer.example.com:9084</li>" + "<li>10.1.2.3</li>" + "<li>10.1.2.3:9084</li>" + "<li>[2001:d8b::1]</li>"
-				+ "<li>[2001:d8b::1]:9084</li>" + "</ul>",
+				+ "<li>some-peer.example.com:9889</li>" + "<li>10.1.2.3</li>" + "<li>10.1.2.3:9889</li>" + "<li>[2001:d8b::1]</li>"
+				+ "<li>[2001:d8b::1]:9889</li>" + "</ul>",
 		requestBody = @RequestBody(
 			required = true,
 			content = @Content(
@@ -162,10 +162,10 @@ public class PeersResource {
 	@DELETE
 	@Operation(
 		summary = "Remove peer address from database",
-		description = "Specify peer to be removed using hostname, IPv4 address, IPv6 address and optional port number preceeded with colon (e.g. :9084)<br>"
+		description = "Specify peer to be removed using hostname, IPv4 address, IPv6 address and optional port number preceeded with colon (e.g. :9889)<br>"
 				+ "Note that IPv6 literal addresses must be surrounded with brackets.<br>" + "Examples:<br><ul>" + "<li>some-peer.example.com</li>"
-				+ "<li>some-peer.example.com:9084</li>" + "<li>10.1.2.3</li>" + "<li>10.1.2.3:9084</li>" + "<li>[2001:d8b::1]</li>"
-				+ "<li>[2001:d8b::1]:9084</li>" + "</ul>",
+				+ "<li>some-peer.example.com:9889</li>" + "<li>10.1.2.3</li>" + "<li>10.1.2.3:9889</li>" + "<li>[2001:d8b::1]</li>"
+				+ "<li>[2001:d8b::1]:9889</li>" + "</ul>",
 		requestBody = @RequestBody(
 			required = true,
 			content = @Content(
