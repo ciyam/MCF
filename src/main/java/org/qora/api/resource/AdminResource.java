e8d442d589462a2604a83a403224af7142a27413
catbref <misc-github@talk2dom.com>
2019-05-24 12:39:26 +0100

    New MCF splash image

--- src/main/java/org/qora/api/resource/AdminResource.java
+++ src/main/java/org/qora/api/resource/AdminResource.java
@@ -25,6 +25,7 @@ import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
