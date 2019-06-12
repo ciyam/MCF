8a17fae926102480147453b24b065bae5f9dd127
catbref <misc-github@talk2dom.com>
2019-06-12 10:51:42 +0100

    Improved rebase and rebuild script

--- rebase-and-rebuild.sh
+++ rebase-and-rebuild.sh
@@ -4,20 +4,27 @@ set -e
 
 commit_msg="Rebased, auto-update JAR"
 
+echo 'Checking for previous JAR commit to remove'
 top_commit=$(git log -n 1 --format=%s)
 if [ "${top_commit}" = "${commit_msg}" ]; then
 	echo 'Removing previous JAR commit'
 	git reset --hard HEAD^
-	git push --force
+	git push --force-with-lease
 fi
 
+echo 'Rebasing using master branch'
 git fetch -p origin
 git rebase origin/master
+
+echo 'Pushing rebased branch'
+git push --force-with-lease
+
+echo 'Building new JAR'
 mvn clean
 mvn package
 cp target/MCF-core*.jar MCF-core.jar
 
+echo 'Pushing new JAR commit'
 git add MCF-core.jar
-
 git commit -m "${commit_msg}"
 git push
